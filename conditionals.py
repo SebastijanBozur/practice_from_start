804e25b96280dd2814808ba227b2e1157cefe311
TeraVita <teravita9@gmail.com>
2023-11-07 18:16:45 +0000

    conditionals

---
+++ conditionals.py
@@ -0,0 +1,32 @@
+# condition = input('enter condition: ').capitalize()
+#
+#
+# if condition == 'Test':
+#     print('Evaluated to True')
+# else:
+#     print('Evaluated to False')
+#
+# language = input('enter programing language: ').capitalize()
+#
+# if language == 'Python':
+#     print('condition met')
+# elif language == 'C+':
+#     print('condition met')
+# elif language == 'Java':
+#     print('condition met')
+# elif language == 'Rust':
+#     print('condition met')
+# else:
+#     print('no match')
+
+a = [1,2,3]
+b = [1,2,3]
+
+print(a is b)
+
+condition = False
+
+if condition:
+    print('Evaluated to True')
+else:
+    print('Evaluated to False')
