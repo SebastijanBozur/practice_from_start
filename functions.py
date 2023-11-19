1a2dc86b054ddfe9d0041416352fc28620d1556d
TeraVita <teravita9@gmail.com>
2023-11-19 18:28:50 +0000

    func update

--- functions.py
+++ functions.py
@@ -8,22 +8,31 @@
 #     print(**kwargs)
 
 # Number of days per month. First value placeholder for indexing purposes.
-month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+# month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#
+#
+# def is_leap(year):
+#     """Return True for leap years, False for non-leap years."""
+#
+#     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+#
+#
+# def days_in_month(year, month):
+#     """Return number of days in that month in that year."""
+#
+#     if not 1 <= month <= 12:
+#         return 'Invalid Month'
+#
+#     if month == 2 and is_leap(year):
+#         return 29
+#
+#     return month_days[month]
 
+def student_info(*args, **kwargs):
+    print(args)
+    print(kwargs)
 
-def is_leap(year):
-    """Return True for leap years, False for non-leap years."""
+courses = ['math', 'biology', 'physics', 'english']
+info = {'fname':'milan','lname':'jakic','age':'26','year':'7'}
 
-    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
-
-
-def days_in_month(year, month):
-    """Return number of days in that month in that year."""
-
-    if not 1 <= month <= 12:
-        return 'Invalid Month'
-
-    if month == 2 and is_leap(year):
-        return 29
-
-    return month_days[month]
+student_info(*courses,**info)
