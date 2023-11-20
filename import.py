418012edd63f7bb2cafa92b2018965196df4358d
TeraVita <teravita9@gmail.com>
2023-11-20 18:28:25 +0000

    modules

---
+++ import.py
@@ -0,0 +1,25 @@
+# from modules import find_index, test
+import random
+import math
+import datetime
+import calendar
+import os
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+
+# index = find_index(courses, 'Math')
+#
+# print(index)
+# print(test)
+
+# random_course = random.choice(courses)
+#
+# print(random_course)
+
+# today = datetime.date.today()
+# print(today)
+#
+# print(calendar.isleap(2023))
+
+print(os.getcwd())
+print(os.__file__)
